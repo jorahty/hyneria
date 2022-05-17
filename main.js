0efa8dfdb972bbc6dde8e7827326895342e8648e
James Tennant <jtennant@ucsc.edu>
2022-05-17 09:37:46 -0700

    config controls

--- main.js
+++ main.js
@@ -1,59 +1,72 @@
+// create 'main' element
 let main = document.createElement('main');
 document.body.appendChild(main);
 scaleApp();
-
 window.onresize = scaleApp;
-function scaleApp() {
-    let windowAspectRatio = window.innerWidth / window.innerHeight;
-    let appAspectRatio = 2 / 3;
-    if (windowAspectRatio > appAspectRatio) {
-        console.log('landscape');
-        main.style.height = (window.innerHeight - 40).toString() + 'px'
-        main.style.width = ((window.innerHeight - 40) * 2 / 3).toString() + 'px';
-    } else {
-        main.style.width = '100%';
-        let height = (window.innerWidth * 3 / 2).toString() + 'px';
-        console.log(height);
-        main.style.height = height
-    }
-}
 
-
-
+// create renderer
 let renderer = document.createElement('canvas');
 renderer.width = 100;
 renderer.height = 100;
 main.appendChild(renderer);
 
+// create controls
 let rotate = document.createElement('button');
 let translate = document.createElement('button');
-let div = document.createElement('div');
-div.appendChild(rotate);
-div.appendChild(translate);
-main.appendChild(div);
+let controlsContainer = document.createElement('div');
+controlsContainer.setAttribute('class','controls-container');
+controlsContainer.appendChild(rotate);
+controlsContainer.appendChild(translate);
+main.appendChild(controlsContainer);
 
-// let displacement = document.createElement('h1');
-// displacement.innerHTML = '&nbsp;';
-// document.body.appendChild(displacement);
+let displacement = document.createElement('h1');
+displacement.style.margin = '0';
+displacement.innerHTML = '&nbsp;';
+main.appendChild(displacement);
 
-// let dragId = 0;
-// let dragStart;
+let dragId = 0;
+let dragStart;
 
-// window.onpointerup = e => {
-//     if (e.pointerId == dragId) {
-//         dragId = 0;
-//     }
-// }
+window.onpointerup = e => {
+    if (e.pointerId == dragId) {
+        dragId = 0;
+        rotate.style.background = '#333';
+    }
+}
 
-// rotate.onpointerdown = e => {
-//     dragId = e.pointerId;
-//     dragStart = e.pageX;
-// }
+translate.onpointerdown = e => {
+    translate.style.background = '#484';
+}
 
-// window.onpointermove = e => {
-//     if (e.pointerId == dragId) {
-//         displacement.textContent = e.pageX - dragStart;
-//     }
-// }
+translate.onpointerup = e => {
+    translate.style.background = '#333';
+}
 
+rotate.onpointerdown = e => {
+    rotate.style.background = '#484';
+    dragId = e.pointerId;
+    dragStart = e.pageX;
+}
 
+window.onpointermove = e => {
+    if (e.pointerId == dragId) {
+        displacement.textContent = e.pageX - dragStart;
+    }
+}
+
+// scaleApp()
+// Sets the size and width of the 'main' element.
+function scaleApp() {
+    let windowAspectRatio = window.innerWidth / window.innerHeight;
+    let appAspectRatio = 2 / 3;
+    let verticalMargin = 40;
+    if (windowAspectRatio > appAspectRatio) {
+        // maximize height, compute width accordingly
+        main.style.height = (window.innerHeight - verticalMargin).toString() + 'px'
+        main.style.width = ((window.innerHeight - verticalMargin) * appAspectRatio).toString() + 'px';
+    } else {
+        // maximize width, compute height accordingly
+        main.style.width = window.innerWidth;
+        main.style.height = (window.innerWidth * 1 / appAspectRatio).toString() + 'px';
+    }
+}
