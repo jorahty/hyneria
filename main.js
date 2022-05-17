fdbe98436c21c042bb23ef906e991aac24f59a95
James Tennant <jtennant@ucsc.edu>
2022-05-17 09:13:58 -0700

    scale app

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
 let main = document.createElement('main');
 document.body.appendChild(main);
+scaleApp();
 
-// let renderer = document.createElement('canvas');
-// document.body.appendChild(renderer);
+window.onresize = scaleApp;
+function scaleApp() {
+    let windowAspectRatio = window.innerWidth / window.innerHeight;
+    let appAspectRatio = 2 / 3;
+    if (windowAspectRatio > appAspectRatio) {
+        console.log('landscape');
+        main.style.height = (window.innerHeight - 40).toString() + 'px'
+        main.style.width = ((window.innerHeight - 40) * 2 / 3).toString() + 'px';
+    } else {
+        main.style.width = '100%';
+        let height = (window.innerWidth * 3 / 2).toString() + 'px';
+        console.log(height);
+        main.style.height = height
+    }
+}
 
-// let rotate = document.createElement('button');
-// let translate = document.createElement('button');
-// let div = document.createElement('div');
-// div.appendChild(rotate);
-// div.appendChild(translate);
-// document.body.appendChild(div);
+
+
+let renderer = document.createElement('canvas');
+renderer.width = 100;
+renderer.height = 100;
+main.appendChild(renderer);
+
+let rotate = document.createElement('button');
+let translate = document.createElement('button');
+let div = document.createElement('div');
+div.appendChild(rotate);
+div.appendChild(translate);
+main.appendChild(div);
 
 // let displacement = document.createElement('h1');
 // displacement.innerHTML = '&nbsp;';
