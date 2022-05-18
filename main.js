f33082b57f0496ca601635b8958c96b8d2ae559d
James Tennant <jtennant@ucsc.edu>
2022-05-17 23:24:48 -0700

    move towards dial

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
-// let gamestate = {
-//     x:
-//     y:
-//     z:
-//     hr:
-//     vr:
-// };
+// define gamestate
+let gamestate = {};
+
+// establish connection to server
+
+// upon recieving update from server, update gamestate
 
 // create 'main' element
 let main = document.createElement('main');
@@ -21,13 +20,6 @@ textureLoader.load('https://cse120.jorahty.repl.co/blue.jpg', texture => {
     scene.background = texture;
 });
 
-// define player geometry
-let geometry = new THREE.ConeGeometry(0.3);
-let material = new THREE.MeshBasicMaterial({ color: 0x020610 });
-let player = new THREE.Mesh(geometry, material);
-player.rotation.x = - Math.PI / 2;
-scene.add(player);
-
 // create camera
 let camera = new THREE.PerspectiveCamera();
 camera.position.z = 5;
@@ -37,25 +29,31 @@ camera.rotation.x = -0.4;
 // create renderer
 let renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(512, 512);
-renderer.setClearColor(0x444444);
 renderer.domElement.style.width = '100%';
 renderer.domElement.style.height = 'auto';
 renderer.outputEncoding = THREE.sRGBEncoding;
 main.appendChild(renderer.domElement);
 
+// define player geometry
+let geometry = new THREE.ConeGeometry(0.3);
+let material = new THREE.MeshBasicMaterial({ color: 0x020610 });
+let player = new THREE.Mesh(geometry, material);
+player.rotation.x = - Math.PI / 2;
+scene.add(player);
+
 // animate
 animate();
 
 // create controls
-let rotate = document.createElement('button');
+let controlsContainer = document.createElement('section');
 let translate = document.createElement('button');
-let controlsContainer = document.createElement('div');
+let dialContainer = document.createElement('div');
+let dial = document.createElement('div');
 configControls();
 
-// scale app (and decorate renderer)
-window.onresize = scaleApp;
-scaleApp();
-setTimeout(scaleApp, 1);
+// adapt to window aspect ratio
+adaptToWindowAspectRatio();
+window.onresize = adaptToWindowAspectRatio;
 
 // ██ helper functions
 
@@ -74,19 +72,38 @@ function update() {
     player.rotation.z += 0.01;
 }
 
-// scaleApp()
-// Sets the size and width of the 'main' element.
-function scaleApp() {
+// // step/simulate the gamestate forward in time (based on input)
+// setInterval(tick, 1000 / 30);
+// function tick() {
+//     // pass
+// }
+
+// configControls()
+// Adds controls to dom
+// Configures controls to listen for input
+function configControls() {
+    main.appendChild(controlsContainer);
+    controlsContainer.appendChild(translate);
+    controlsContainer.appendChild(dialContainer);
+    dialContainer.appendChild(dial);
+
+    controlsContainer.setAttribute('class','controls-container');
+    dialContainer.setAttribute('class','dial-container');
+    dial.setAttribute('class','.dial');
+}
+
+function adaptToWindowAspectRatio() {
     let windowAspectRatio = window.innerWidth / window.innerHeight;
-    let appAspectRatio = 2 / 3.3;
+    let appAspectRatio = 0.6;
+    let topMargin = 0.013 * window.innerHeight;
     if (windowAspectRatio > appAspectRatio) {
         // maximize height, compute width accordingly
-        main.style.height = (window.innerHeight).toString() + 'px'
-        main.style.width = ((window.innerHeight) * appAspectRatio).toString() + 'px';
+        main.style.height = (window.innerHeight - topMargin).toString() + 'px';
+        main.style.width = ((window.innerHeight - topMargin) * appAspectRatio).toString() + 'px';
         renderer.domElement.style.borderWidth = '2px';
-        renderer.domElement.style.borderRadius = '20px';
+        renderer.domElement.style.borderRadius = '2vh';
         controlsContainer.style.padding = '1.3vh 0';
-        main.style.padding = '0';
+        main.style.marginTop = (topMargin).toString() + 'px';
     } else {
         // maximize width, compute height accordingly
         main.style.width = (window.innerWidth).toString() + 'px';
@@ -94,60 +111,6 @@ function scaleApp() {
         renderer.domElement.style.borderWidth = '2px 0 2px 0';
         renderer.domElement.style.borderRadius = '0';
         controlsContainer.style.padding = '1.3vh';
-        main.style.padding = '0';
-    }
-}
-
-// on touch move,
-//  if touch id is the drag id
-//     compute panOffset (from initPanPosition)
-//     use offset to set playerRotation relative to initPlayerRotation
-//     keep vertical rotation in range
-
-
-// controls should listen for input,
-// and modify the *gamestate* accordingly
-function configControls() {
-    rotate.setAttribute('class','rotate');
-    controlsContainer.setAttribute('class','controls-container');
-    controlsContainer.appendChild(rotate);
-    controlsContainer.appendChild(translate);
-    main.appendChild(controlsContainer);
-
-    let displacement = document.createElement('h1');
-    displacement.style.margin = '0';
-    displacement.innerHTML = '&nbsp;';
-    displacement.style.position = 'absolute';
-    displacement.style.top = '0%';
-    document.body.appendChild(displacement);
-
-    let dragId = 0;
-    let dragStart;
-
-    window.onpointerup = e => {
-        if (e.pointerId == dragId) {
-            dragId = 0;
-            rotate.style.background = '#333';
-        }
-    }
-
-    translate.onpointerdown = e => {
-        translate.style.background = '#484';
-    }
-
-    translate.onpointerup = e => {
-        translate.style.background = '#333';
-    }
-
-    rotate.onpointerdown = e => {
-        rotate.style.background = '#484';
-        dragId = e.pointerId;
-        dragStart = e.pageX;
-    }
-
-    window.onpointermove = e => {
-        if (e.pointerId == dragId) {
-            displacement.textContent = e.pageX - dragStart;
-        }
+        main.style.marginTop = '0';
     }
 }
