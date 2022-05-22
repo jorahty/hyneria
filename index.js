42d02c21c1a182e5f6b171a8e8996d2904ddab8b
James Tennant <jtennant@ucsc.edu>
2022-05-22 15:51:28 -0700

    create threejs scene on the server!

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const socketio = require("socket.io");
+const THREE = require("three");
 
 const app = express();
 const httpserver = http.Server(app);
@@ -11,8 +12,10 @@ app.use(express.static(clientDirectory)); // Serve client folder
 const port = process.env.PORT || 3000;
 httpserver.listen(port);
 
+let scene = new THREE.Scene();
+
 io.on('connection', socket => {
 
   console.log('socket.id', socket.id);
-  
+
 });
