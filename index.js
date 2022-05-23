c1379f71ef68f0bca446f44c91dd7182ae34f1a1
James Tennant <jtennant@ucsc.edu>
2022-05-23 00:24:17 -0700

    controls work

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ httpserver.listen(port);
 
 const BROADCAST_RATE = 30;
 const TICK_RATE = 30;
+const TRANSLATE_SPEED = 0.1;
+const ROTATE_SPEED = 0.1;
 
 // globals
 let gamestate = {};
@@ -52,29 +54,35 @@ io.on('connection', socket => {
 // emit regular updates to all clients
 setInterval(() => { io.volatile.emit('update', gamestate); }, 1000 / BROADCAST_RATE);
 
-// step/simulate the gamestate forward in time (based on input)
+// step/simulate the gamestate forward in time (based on controls)
 setInterval(Tick, 1000 / TICK_RATE);
 function Tick() {
   for (let id in gamestate) { // for every user
 
-    if (controls[id].translate) {
-      gamestate[id].p[0] += 0.1;
-    }
-
+    // rotate
     if (controls[id].rotate.includes('w')) {
-      gamestate[id].r[1] = Math.min(gamestate[id].r[1] + 0.1, Math.PI / 2 - 0.1);
+      gamestate[id].r[1] = Math.min(gamestate[id].r[1] + ROTATE_SPEED, Math.PI / 2 - 0.1);
     }
 
     if (controls[id].rotate.includes('s')) {
-      gamestate[id].r[1] = Math.max(gamestate[id].r[1] - 0.1, -Math.PI / 2 + 0.1);
+      gamestate[id].r[1] = Math.max(gamestate[id].r[1] - ROTATE_SPEED, -Math.PI / 2 + 0.1);
     }
 
     if (controls[id].rotate.includes('a')) {
-      gamestate[id].r[0] += 0.1;
+      gamestate[id].r[0] += ROTATE_SPEED;
     }
 
     if (controls[id].rotate.includes('d')) {
-      gamestate[id].r[0] -= 0.1;
+      gamestate[id].r[0] -= ROTATE_SPEED;
+    }
+
+    // translate
+    if (controls[id].translate) {
+      let phi = gamestate[id].r[0];
+      let theta = gamestate[id].r[1] + Math.PI / 2;
+      gamestate[id].p[0] -= TRANSLATE_SPEED * Math.sin(phi) * Math.sin(theta);
+      gamestate[id].p[1] -= TRANSLATE_SPEED * Math.cos(theta);
+      gamestate[id].p[2] -= TRANSLATE_SPEED * Math.cos(phi) * Math.sin(theta);
     }
   }
 }
